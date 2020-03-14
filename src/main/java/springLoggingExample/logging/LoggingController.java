7566879c015b7b1a084b8537609630b553d77ed9
Tamas Szirbucz <szirbucz@kodekonveyor.com>
2020-03-14 12:50:07 +0100

    Implement log endpoint

--- src/main/java/springLoggingExample/logging/LoggingController.java
+++ src/main/java/springLoggingExample/logging/LoggingController.java
@@ -1,5 +1,7 @@
 package springLoggingExample.logging;
 
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,7 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LoggingController {
 	
+	@Autowired
+	private Logger logger;
+	
 	@GetMapping("/log/{message}")
 	public void info(@PathVariable("message") final String message) {
+		logger.info(message);
 	}
 }
