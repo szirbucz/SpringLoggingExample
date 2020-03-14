87eccebdda90eb2fd816f4e2f45340f38838dc2a
Tamas Szirbucz <szirbucz@kodekonveyor.com>
2020-03-14 12:56:41 +0100

    Implement marked logger endpoint

--- src/main/java/springLoggingExample/logging/MarkedLoggingController.java
+++ src/main/java/springLoggingExample/logging/MarkedLoggingController.java
@@ -1,13 +1,20 @@
 package springLoggingExample.logging;
 
+import org.slf4j.Logger;
+import org.slf4j.MarkerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MarkedLoggingController {
+
+	@Autowired
+	private Logger logger;
 	
 	@GetMapping("/o1g/{message}")
 	public void o1g(@PathVariable("message") final String message) {
+		logger.info(MarkerFactory.getMarker("o1g"), message);
 	}
 }
