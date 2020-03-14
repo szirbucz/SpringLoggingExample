e871f4fa146e3285c5a0264ac47f4d240567844e
Tamas Szirbucz <szirbucz@kodekonveyor.com>
2020-03-14 12:43:25 +0100

    Add Logger prototype bean

--- src/main/java/springLoggingExample/Application.java
+++ src/main/java/springLoggingExample/Application.java
@@ -1,7 +1,16 @@
 package springLoggingExample;
 
+import java.lang.reflect.Field;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InjectionPoint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.core.MethodParameter;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +19,16 @@ public class Application {
 	    SpringApplication.run(Application.class, args);
 	  }
 
+	@Bean
+	@Scope("prototype")
+	public Logger logger(final InjectionPoint ip) {
+		return LoggerFactory.getLogger(Optional.ofNullable(ip.getMethodParameter())
+				.<Class<?>>map(MethodParameter::getContainingClass)
+				.orElseGet( () ->
+				Optional.ofNullable(ip.getField())
+				.map(Field::getDeclaringClass)   
+				.orElseThrow (IllegalArgumentException::new)
+						)
+				);
+	}
 }
